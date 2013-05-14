f68c379d412d1a74d89f9fd4014e6ea8bd38ece0
zhandong <zhandong.liu@bcm.edu>
2013-05-14 11:46:52 -0500

    Inital Commit

---
+++ src/CRFfunctions.R
@@ -0,0 +1,131 @@
+####################
+#functions for conditional random fields
+
+require('huge')
+require('glmnet')
+require('multicore')
+
+
+
+########internal function
+mykron = function(vec,ind){kronecker(vec[ind],vec[-ind])}
+
+
+#################
+#neighborhood selection for Gaussian linear combo model
+#assumes Y is n x p and X is n x q & node is the index of the node of interest
+#each of the covariates X should be of the same type
+#as the corresponding coeffiicents are regularized by the same amount
+#Returns:
+#lams = sequence of lambda values
+#CoefMat = the full coefficient matrix - (p-1)*q x nlams
+#Bhat = coefficients arranged as a 3D array: (p-1) x q x nlams
+GGM.linearCRF.path.neighborhood = function(Y,X,node,nlams)
+{
+  ind = (ncol(X)+1):(ncol(X)+ncol(Y)-1)
+  XY = t(apply(cbind(X,Y[,-node]),1,mykron,ind))
+  fit = glmnet(XY,Y[,node],family="gaussian",nlambda=nlams)
+  return(list(lams=fit$lambda,Coefhat=fit$beta,Bhat=array(fit$beta,c(ncol(Y)-1,ncol(X),nlams))))  
+}
+
+
+
+#################
+#neighborhood selection for Gaussian mean model
+#assumes Y is n x p and X is n x q & node is the index of the node of interest
+#each of the covariates X should be of the same type
+#as the corresponding coeffiicents are regularized by the same amount
+#alpha is a scalar that differentially penalizes the coefficients corresponding to X
+#penalty term = \lambda*\alpha ||\beta||_1 + \lambda||\theta||_1
+#Returns:
+#lams = sequence of lambda values
+#CoefMat = the full coefficient matrix - (p-1 + q) x nlams
+#Bhat = coefficients corresponding to X's - q x nlams
+#Thethat = coefficients corresponding to Y's - (p-1) x nlams
+GGM.meanCRF.path.neighborhood = function(Y,X,node,nlams,alpha)
+{
+  pfac = c(rep(1,(ncol(Y)-1)),rep(alpha,ncol(X)))
+  fit = glmnet(cbind(Y[,-node],X),Y[,node],family="gaussian",nlambda=nlams,penalty.factor=pfac)
+  return(list(lams=fit$lambda,CoefMat=fit$beta,Thethat=fit$beta[1:(p-1),],Bhat=fit$beta[p:(p-1+q),]))    
+}
+
+
+#################
+#neighborhood selection for Poisson mean model
+#assumes Y is n x p and X is n x q & node is the index of the node of interest
+#each of the covariates X should be of the same type
+#as the corresponding coeffiicents are regularized by the same amount
+#alpha is a scalar that differentially penalizes the coefficients corresponding to X
+#penalty term = \lambda*\alpha ||\beta||_1 + \lambda||\theta||_1
+#Note that both B and Theta are non-positive
+#Returns:
+#lams = sequence of lambda values
+#CoefMat = the full coefficient matrix - (p-1 + q) x nlams
+#Bhat = coefficients corresponding to X's - q x nlams
+#Thethat = coefficients corresponding to Y's - (p-1) x nlams
+PGM.meanCRF.path.neighborhood = function(x,y,node,alpha,nlams,lmax,startb=0)
+{
+  X = cbind(y[,-node],x); Y = y[,node];
+  pfac = c(1,rep(1,(ncol(y)-1)),rep(alpha,ncol(x)))
+  n = nrow(X); p = ncol(X);
+  lams = exp(seq(log(lmax),log(.0001),l=nlams));
+  if(nlams==1){lams = lmax};
+  thr = 1e-6; maxit = 1e6;
+  Xt = cbind(t(t(rep(1,n))),X);
+  L = max(eigen(t(X)%*%X,only.values=TRUE)$values);
+  alphas = 0; Bmat = matrix(0,p,nlams);
+  if(sum(startb)==0){Bhat = matrix(runif(p+1),p+1,1); Bhat[1] = 0}else{Bhat=startb}
+  for(i in 1:nlams){
+    iter = 1; ind = 1;
+    while(thr<ind & iter<maxit){
+      oldb = Bhat;
+      tmp = Bhat - (t(Xt)%*%Y - t(Xt)%*%exp(-Xt%*%Bhat))/L;
+      Bhat = matrix(sapply(tmp - pfac*lams[i]/L,max,0),p+1,1); Bhat[1] = tmp[1];
+      ind = sum((Bhat - oldb)^2);
+      iter = iter + 1;
+    }
+    alphas[i] = -Bhat[1];
+    Bmat[,i] = -Bhat[2:(p+1),drop=FALSE];
+  }
+  return(list(ahat=alphas,CoefMat=Bmat,lams=lams,Thethat=Bmat[1:(ncol(y)-1),],Bhat=Bmat[ncol(y):(ncol(y)-1+ncol(x)),]))
+}
+
+
+
+#################
+#neighborhood selection for Poisson linear combo model
+#assumes Y is n x p and X is n x q & node is the index of the node of interest
+#each of the covariates X should be of the same type
+#as the corresponding coeffiicents are regularized by the same amount
+#Note that both all coefficients as non-positive
+#Returns:
+#lams = sequence of lambda values
+#CoefMat = the full coefficient matrix - (p-1)*q x nlams
+#Bhat = coefficients arranged as a 3D array: (p-1) x q x nlams
+PGM.linearCRF.path.neighborhood = function(x,y,node,nlams,lmax,startb=0)
+{
+  ind = (ncol(x)+1):(ncol(x)+ncol(y)-1)
+  X = t(apply(cbind(x,y[,-node]),1,mykron,ind))
+  Y = y[,node]
+  n = nrow(X); p = ncol(X);
+  lams = exp(seq(log(lmax),log(.0001),l=nlams));
+  if(nlams==1){lams = lmax};
+  thr = 1e-6; maxit = 1e6;
+  Xt = cbind(t(t(rep(1,n))),X);
+  L = max(eigen(t(X)%*%X,only.values=TRUE)$values);
+  alphas = 0; Bmat = matrix(0,p,nlams);
+  if(sum(startb)==0){Bhat = matrix(runif(p+1),p+1,1); Bhat[1] = 0}else{Bhat=startb}
+  for(i in 1:nlams){
+    iter = 1; ind = 1;
+    while(thr<ind & iter<maxit){
+      oldb = Bhat;
+      tmp = Bhat - (t(Xt)%*%Y - t(Xt)%*%exp(-Xt%*%Bhat))/L;
+      Bhat = matrix(sapply(tmp - lams[i]/L,max,0),p+1,1); Bhat[1] = tmp[1];
+      ind = sum((Bhat - oldb)^2);
+      iter = iter + 1;
+    }
+    alphas[i] = -Bhat[1];
+    Bmat[,i] = -Bhat[2:(p+1),drop=FALSE];
+  }
+  return(list(ahat=alphas,CoefMat=Bmat,lams=lams,Bhat=array(Bmat,c(ncol(y)-1,ncol(x),nlams))))
+}
